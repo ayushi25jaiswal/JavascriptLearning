ed777d9975a433525cb88933012141164845d6d5
ayushi jaiswal <ayushijaiswal2507@gmail.com>
2023-10-09 20:22:10 +0530

    Short Circuiting & Challenge 1

--- data_strucures/script.js
+++ data_strucures/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
+// // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -42,10 +42,10 @@ const restaurant = {
   },
 };
 
-// const ingridents = [prompt('Ing 1'), prompt('Ing 2'), prompt('Ing3')];
+// // const ingridents = [prompt('Ing 1'), prompt('Ing 2'), prompt('Ing3')];
 // restaurant.oredrPasta(...ingridents); //SPREAD OPERATOR
 
-restaurant.orderPizza('Paneer', 'olive', 'onion', 'cheese');
+// restaurant.orderPizza('Paneer', 'olive', 'onion', 'cheese');
 
 // const arr = [2, 3, 4];
 // const newArr = [0, 1, ...arr];
@@ -64,3 +64,139 @@ restaurant.orderPizza('Paneer', 'olive', 'onion', 'cheese');
 
 // const { name: restaurantName, openingHours: hours } = restaurant;
 // console.log(restaurantName, hours);
+
+//////////////////////////////////////////SHORT CIRCUITING/////////////////////////
+
+//OR OPERATOR ||
+
+restaurant.numGuest = 0;
+const guest = restaurant.numGuest ? restaurant.numGuest : 10;
+console.log(guest);
+
+const otherGuest = restaurant.numGuest || 10; // better than using ternary operator
+console.log(otherGuest);
+
+//&& Operator
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('panner', 'cheese');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('panner', 'olive');
+
+// ?? NULLISH OPERATOR
+
+const guestCorrect = restaurant.newGuest ?? 10;
+console.log(guestCorrect);
+
+// Logical Assignment Operator
+restaurant.newGuest = 0;
+//OR assgingment operator
+restaurant.newGuest ||= 10;
+
+//nullish assignmet operator
+restaurant.newGuest ??= 111;
+
+//AND assignment Operator
+restaurant.owner = 'Jonas';
+restaurant.owner &&= 'ANOYNOMus';
+
+console.log(restaurant.owner);
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna 
+work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern 
+Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with 
+all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') 
+
+containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints
+ each of them to the console, along with the number of goals that were scored in total (number of player names 
+passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win,
+ WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again
+ with players from game.scored
+
+GOOD LUCK 😀
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [player1, player2] = game.players;
+// console.log(player1);
+// console.log(player2);
+
+const [gk1, ...fieldPlayers1] = player1;
+// console.log(gk1, fieldPlayers1);
+
+const [gk2, ...fieldPlayers2] = player2;
+// console.log(gk2, fieldPlayers2);
+
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+const player1Final1 = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(player1Final1);
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} scored goals`);
+};
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 will Win');
+team1 > team2 && console.log('Team 2 will Win');
