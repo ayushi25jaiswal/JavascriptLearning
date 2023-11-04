21b467a44aa69524e66ebb81b51f59f32a0d1336
ayushi jaiswal <ayushijaiswal2507@gmail.com>
2023-11-04 14:24:33 +0530

    Challenge-3

--- Asyncronous/script.js
+++ Asyncronous/script.js
@@ -390,81 +390,180 @@ const getLocation = function () {
 // })();
 
 ///////////////////////////RUN PROMISES PARALLEL/////////////////////////
-const getJSON = function (url, errorMsg) {
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(`${errorMsg} ${response.status}`);
-    }
-    return response.json();
+// const getJSON = function (url, errorMsg) {
+//   return fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(`${errorMsg} ${response.status}`);
+//     }
+//     return response.json();
+//   });
+// };
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+//     // console.log([data1.capital, data2.capital, data3.capital]);
+
+//     //RUn PARALLEL
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+//     ]);
+//     // console.log(data.map(d => d[0].capitals));
+//     console.log(typeof data);
+//     console.log(data.map(d => d[0].capital));
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// };
+
+// get3Countries('japan', 'india', 'nepal');
+
+// ///////////////////////////////Promise Combinataors//////////////////////////////
+// (async function () {
+//   const res = await Promise.race([
+//     getJSON(`https://restcountries.com/v3.1/name/india`),
+//     getJSON(`https://restcountries.com/v3.1/name/japan`),
+//     getJSON(`https://restcountries.com/v3.1/name/nepal`),
+//   ]);
+//   console.log(res[0]);
+// })();
+
+// const timeout = function (sec) {
+//   return new Promise(function (_, reject) {
+//     setTimeout(function () {
+//       reject(new Error('Took To LOOOOOOOOOOOOOOOOOOng'));
+//     }, sec * 1000);
+//   });
+// };
+
+// Promise.race([getJSON(`https://restcountries.com/v3.1/name/japan`), timeout(2)])
+//   .then(res => console.log(res[0]))
+//   .catch(err => console.error(err.message));
+
+// Promise.allSettled([
+//   Promise.resolve('SUCCESS'),
+//   Promise.reject('ERROR'),
+//   Promise.resolve('Another Success'),
+// ]).then(res => console.log(res));
+
+// Promise.all([
+//   Promise.resolve('SUCCESS'),
+//   Promise.reject('ERROR'),
+//   Promise.resolve('Another Success'),
+// ])
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err));
+
+// Promise.any([
+//   Promise.reject('ERROR'),
+//   Promise.reject('ERROR'),
+//   Promise.reject('ERROR'),
+//   Promise.resolve('SUCCESS'),
+//   Promise.reject('ERROR'),
+//   Promise.resolve('Another Success'),
+// ])
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err));
+
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using
+ async/await (only the part where the promise is consumed). Compare the two versions, think about
+  the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the 
+  resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
   });
 };
-const get3Countries = async function (c1, c2, c3) {
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+//   .catch(err => console.error(err));
+
+const loadNPause = async function () {
   try {
-    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 Loaded');
+    await wait(2);
+    img.style.display = 'none';
 
-    // console.log([data1.capital, data2.capital, data3.capital]);
-
-    //RUn PARALLEL
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-    ]);
-    // console.log(data.map(d => d[0].capitals));
-    console.log(typeof data);
-    console.log(data.map(d => d[0].capital));
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 Loaded');
+    await wait(2);
+    img.style.display = 'none';
   } catch (err) {
-    console.error(err.message);
+    console.log(err);
   }
 };
 
-get3Countries('japan', 'india', 'nepal');
+loadNPause();
 
-///////////////////////////////Promise Combinataors//////////////////////////////
-(async function () {
-  const res = await Promise.race([
-    getJSON(`https://restcountries.com/v3.1/name/india`),
-    getJSON(`https://restcountries.com/v3.1/name/japan`),
-    getJSON(`https://restcountries.com/v3.1/name/nepal`),
-  ]);
-  console.log(res[0]);
-})();
-
-const timeout = function (sec) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error('Took To LOOOOOOOOOOOOOOOOOOng'));
-    }, sec * 1000);
-  });
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    const imgEl = Promise.all(imgs);
+    console.log(imgEl);
+    imgEl.forEach(img => (img.classList.add = 'parallel'));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-Promise.race([getJSON(`https://restcountries.com/v3.1/name/japan`), timeout(2)])
-  .then(res => console.log(res[0]))
-  .catch(err => console.error(err.message));
-
-Promise.allSettled([
-  Promise.resolve('SUCCESS'),
-  Promise.reject('ERROR'),
-  Promise.resolve('Another Success'),
-]).then(res => console.log(res));
-
-Promise.all([
-  Promise.resolve('SUCCESS'),
-  Promise.reject('ERROR'),
-  Promise.resolve('Another Success'),
-])
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
-
-Promise.any([
-  Promise.reject('ERROR'),
-  Promise.reject('ERROR'),
-  Promise.reject('ERROR'),
-  Promise.resolve('SUCCESS'),
-  Promise.reject('ERROR'),
-  Promise.resolve('Another Success'),
-])
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
